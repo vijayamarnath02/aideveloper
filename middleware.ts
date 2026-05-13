66b5b5832f8677330a69af2c617649a2877e62dc
Vijay Amarnath MV <vijayamarnath.mv@purpleslate.in>
2026-05-13 15:36:39 +0530

    feat: add user details API and dashboard page with JWT authentication middleware

--- middleware.ts
+++ middleware.ts
@@ -1,33 +1,64 @@
 export const runtime = "nodejs";
+
 import { NextRequest, NextResponse } from "next/server";
 import { verifyToken } from "./app/lib/jwt";
-
-const secret = new TextEncoder().encode(process.env.JWT_SECRET);
+import { cookies } from "next/headers";
 
 export async function middleware(request: NextRequest) {
   try {
-    // Get token from cookie
-    const token = request.cookies.get("token")?.value;
-    console.log("Token from cookie:", token);
+    const path = request.nextUrl.pathname;
 
-    // No token
-    if (!token) {
-      return NextResponse.redirect(new URL("/auth/login", request.url));
+    // Auth pages
+    const isAuthPage =
+      path === "/auth/login" || path === "/auth/signup" || path === "/"; // Allow home page for logged in users as well
+
+    // Protected pages
+    const isProtectedRoute = path.startsWith("/dashboard");
+
+    // Get token
+    const token = request.cookies.get("token")?.value;
+
+
+    // Verify token if exists
+    const verifiedToken = token ? await verifyToken(token) : null;
+if (!verifiedToken) {
+  (await cookies()).delete("token");
+}
+    // =====================================
+    // USER ALREADY LOGGED IN
+    // =====================================
+    // Prevent access to login/signup
+    if (isAuthPage && verifiedToken) {
+      return NextResponse.redirect(
+        new URL("/dashboard", request.url)
+      );
     }
-    const  verfyTok = await verifyToken(token);
-    console.log("Token is valid:", verfyTok);
-    if (!verfyTok) {
-      return NextResponse.redirect(new URL("/auth/login", request.url));
+
+    // =====================================
+    // USER NOT LOGGED IN
+    // =====================================
+    // Protect dashboard routes
+    if (isProtectedRoute && !verifiedToken) {
+      return NextResponse.redirect(
+        new URL("/auth/login", request.url)
+      );
     }
+
+    // Allow request
     return NextResponse.next();
   } catch (error) {
-    console.log("Invalid Token");
+    console.log("Middleware Error:", error);
 
-    // Token invalid or expired
-    return NextResponse.redirect(new URL("/auth/login", request.url));
+    return NextResponse.redirect(
+      new URL("/auth/login", request.url)
+    );
   }
 }
 
 export const config = {
-  matcher: ["/dashboard/:path*"],
+  matcher: [
+    "/auth/:path",
+    "/dashboard/:path*",
+    "/"
+  ],
 };
